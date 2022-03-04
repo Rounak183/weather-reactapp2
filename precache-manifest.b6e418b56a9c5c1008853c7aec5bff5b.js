267980e9b056712e426331865a2b91632e8f96d3
Rounak183 <rounaksaraf.official@gmail.com>
2022-03-05 01:20:40 +0530

    Updates

---
+++ precache-manifest.b6e418b56a9c5c1008853c7aec5bff5b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0885c08aa9bbefc274d9a2dc30e0de2a",
+    "url": "/weather-reactapp2/index.html"
+  },
+  {
+    "revision": "942bd8889a199022fb23",
+    "url": "/weather-reactapp2/static/css/main.e6054fdf.chunk.css"
+  },
+  {
+    "revision": "2a1b54c4524809c4681c",
+    "url": "/weather-reactapp2/static/js/2.0cca932c.chunk.js"
+  },
+  {
+    "revision": "942bd8889a199022fb23",
+    "url": "/weather-reactapp2/static/js/main.ecb22466.chunk.js"
+  },
+  {
+    "revision": "e5fa8f0eff9ceb639cdd",
+    "url": "/weather-reactapp2/static/js/runtime-main.255335f0.js"
+  },
+  {
+    "revision": "c97cb84b74a52edcb496295687e87822",
+    "url": "/weather-reactapp2/static/media/cold-bg.c97cb84b.jpg"
+  },
+  {
+    "revision": "635ed87d2cc826fb213057ac40dfaff1",
+    "url": "/weather-reactapp2/static/media/warm-bg.635ed87d.jpg"
+  }
+]);
